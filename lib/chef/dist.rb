732e59f027657ad2d4da19f086dcbcdbdbd3c7ca
fw2568 <f.wagner@wagnersolutions.de>
2019-08-20 13:43:25 +0200

    remove chef trademarks

--- lib/chef/dist.rb
+++ lib/chef/dist.rb
@@ -2,49 +2,49 @@ class Chef
   class Dist
     # This class is not fully implemented, depending on it is not recommended!
     # When referencing a product directly, like Chef (Now Chef Infra)
-    PRODUCT = "Chef Infra Client".freeze
+    PRODUCT = "dbosoft NC Client".freeze
 
     # The name of the server product
-    SERVER_PRODUCT = "Chef Infra Server".freeze
+    SERVER_PRODUCT = "dbosoft NC Server".freeze
 
     # The client's alias (chef-client)
-    CLIENT = "chef-client".freeze
+    CLIENT = "dbonc-client".freeze
 
     # name of the automate product
-    AUTOMATE = "Chef Automate".freeze
+    AUTOMATE = "dbosoft NC Automate".freeze
 
     # The chef executable, as in `chef gem install` or `chef generate cookbook`
-    EXEC = "chef".freeze
+    EXEC = "dbonc".freeze
 
     # product website address
-    WEBSITE = "https://chef.io".freeze
+    WEBSITE = "https://dbosoft.eu".freeze
 
     # Chef-Zero's product name
-    ZERO = "Chef Infra Zero".freeze
+    ZERO = "dbosoft NC Zero".freeze
 
     # Chef-Solo's product name
-    SOLO = "Chef Infra Solo".freeze
+    SOLO = "dbosoft NC Solo".freeze
 
     # The chef-zero executable (local mode)
-    ZEROEXEC = "chef-zero".freeze
+    ZEROEXEC = "dbonc-zero".freeze
 
     # The chef-solo executable (legacy local mode)
-    SOLOEXEC = "chef-solo".freeze
+    SOLOEXEC = "dbonc-solo".freeze
 
     # The chef-shell executable
-    SHELL = "chef-shell".freeze
+    SHELL = "dbonc-shell".freeze
 
     # Configuration related constants
     # The chef-shell configuration file
-    SHELL_CONF = "chef_shell.rb".freeze
+    SHELL_CONF = "dbonc_shell.rb".freeze
 
     # The configuration directory
     CONF_DIR = "/etc/#{Chef::Dist::EXEC}".freeze
 
     # The user's configuration directory
-    USER_CONF_DIR = ".chef".freeze
+    USER_CONF_DIR = ".dbonc".freeze
 
     # The server's configuration directory
-    SERVER_CONF_DIR = "/etc/chef-server".freeze
+    SERVER_CONF_DIR = "/etc/dbonc-server".freeze
   end
 end
